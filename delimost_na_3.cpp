d48befdfc3a3ea46a8e51128a8f9609fdc2a3d8f
frosty12 <38581029+frosty12@users.noreply.github.com>
2019-03-05 18:31:43 +0300

    Update delimost_na_3.cpp

--- delimost_na_3.cpp
+++ delimost_na_3.cpp
@@ -1,23 +1,19 @@
 #include <iostream>
-
 int a,b,c;
 using namespace std;
 
 int main() {
-	cout  << "Enter first number \n";
-	cin>>a;
-	cout  << "Enter second number \n";
-	cin>>b;
-	cout  << "Enter third number \n";
-	cin>>c;
-	if ((a+b+c)%3==0)
-	{
-		cout<<"number is divided"<<endl;
+	std::cout  << "Enter first number \n";
+	std::cin >> a;
+	std::cout  << "Enter second number \n";
+	std::cin >> b;
+	std::cout  << "Enter third number \n";
+	std::cin >> c;
+	if ((a+b+c)%3==0){
+		std::cout << "number is divided" << std::endl;
 	}
-	else
-	{
-		cout<<"number is not divided"<<endl;
-		
+	else{
+		std::cout << "number is not divided" << std::endl;	
 	}
 	return 0;
 }
