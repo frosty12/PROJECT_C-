57239e3cde1cea30ca4fb77a9fd5c9c264b35c79
frosty12 <38581029+frosty12@users.noreply.github.com>
2019-03-05 20:45:14 +0300

    Исправлено формат

--- from_min_to_max.cpp
+++ from_min_to_max.cpp
@@ -1,64 +1,59 @@
 #include <iostream>
-int a,b,c;
+int a, b, c;
 using namespace std;
 
 
-int main () {
-	cout  << "Enter first number \n";
-	cin>>a;
-	cout  << "Enter second number \n";
-	cin>>b;
-	cout  << "Enter third number \n";
-	cin>>c;
-	if (a>b)
-	{
-		if (a>c)
-		{
-			if (b>c)
-			{
-				cout<<" min is "<<c<<endl;
-				cout<<" middle number is "<<b<<endl;
+int main() {
+	std::cout << "Enter first number \n";
+	std::cin >> a;
+	std::cout << "Enter second number \n";
+	std::cin >> b;
+	std::cout << "Enter third number \n";
+	std::cin >> c;
+
+	if (a > b) {
+		if (a > c) {
+			if (b > c) {
+				std::cout << " min is " << c << std::endl;
+				std::cout << " middle number is " << b << std::endl;
+
 			}
-			else
-			{
-				cout<<" min is "<<b<<endl;
-				cout<<" middle number is "<<c<<endl;
+			else {
+				std::cout << " min is " << b << endl;
+				std::cout << " middle number is " << c << std::endl;
+
 			}
-				cout<<" max is "<<a<<endl;
-		} 
-		else
-		{
-		
-		cout<<" min is "<<b<<endl;
-		cout<<" middle number is "<<a<<endl;	
-		cout<<" max is "<<c<<endl;
-		
+			std::cout << " max is " << a << std::endl;
+
 		}
-		
+		else {
+			std::cout << " min is " << b << std::endl;
+			std::cout << " middle number is " << a << std::endl;
+			std::cout << " max is " << c << std::endl;
+		}
+
 	}
 	else {
-	
-		if (a>c)
-		{
-		cout<<" min is "<<c<<endl;
-		cout<<" middle number is "<<a<<endl;
-		cout<<" max is "<<b<<endl;	
+		if (a > c) {
+			std::cout << " min is " << c << std::endl;
+			std::cout << " middle number is " << a << endl;
+			std::std::cout << '\n'; << " max is " << b << endl;
 		}
-	    else 
-	    {
-	    cout<<" min is "<<a<<endl;
-		if (b>c)
-		{
-		cout<<" middle number is "<<c<<endl;
-		cout<<" max is "<<b<<endl;	
+		else {
+			std::cout << " min is " << a << endl;
+			if (b > c){
+				std::cout << " middle number is " << c << endl;
+				std::cout << " max is " << b << endl;
+			}
+			else{
+				std::cout << " middle number is " << b << endl;
+				cout << " max is " << c << endl;
+
+			}
+
 		}
-		else
-		{
-		cout<<" middle number is "<<b<<endl;
-		cout<<" max is "<<c<<endl;
-			
-		}	
-	    }
-}
-return 0;
+
+	}
+	return 0;
+
 }
