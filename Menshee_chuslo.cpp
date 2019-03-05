9f9d8c1faf4a485f9a1a6777104438cb6f802e81
frosty12 <38581029+frosty12@users.noreply.github.com>
2019-03-05 18:26:25 +0300

    Update Menshee_chuslo.cpp

--- Menshee_chuslo.cpp
+++ Menshee_chuslo.cpp
@@ -4,34 +4,25 @@ using namespace std;
 int main () {
 int a,b,c,x;
         std: :cout  << "Enter first number \n";
-	cin>>a;
-	cout  << "Enter second number \n";
-	cin>>b;
-	cout  << "Enter third number \n";
-	cin>>c;
+	std::cin >> a;
+	std::cout  << "Enter second number \n";
+	std::cin >> b;
+	std::cout  << "Enter third number \n";
+	std::cin >> c;
 	x = a;
-	if (x<=b)
-    {
-
+	if (x<=b){
 		;
-	
 	}
-	else
-	{
-		x = b;
-		
+	else{
+		x = b;	
 	}
-	if (x<=c)
-	{
-	cout<<"min is "<<x<<endl;
-	cin>>x;
-	
+	if (x<=c){
+	std::cout << "min is " << x << std::endl;
+	cin >> x;
 	}
-	else
-	{
-	cout<<"min is "<<c<<endl;
-	cin>>c;
-		
+	else{
+	std::cout << "min is " << c << std::endl;
+	std::cin >> c;
 	}
 	return 0;
 }
