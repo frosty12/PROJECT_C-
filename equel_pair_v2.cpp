932c196a675edec9799d8937feb773da9b2246d4
frosty12 <38581029+frosty12@users.noreply.github.com>
2019-03-08 15:03:16 +0300

    Create equel_pair_v2.cpp

---
+++ equel_pair_v2.cpp
@@ -0,0 +1,25 @@
+#include "stdafx.h"
+#include <iostream>
+
+int main(int argc, _TCHAR* argv[])
+{
+	int i, c, k, cnt,prv_i;
+	c = 0;
+	k = 0;
+	cnt = 0;
+	while (true) {
+		std::cout << "Enter number " << std::endl;
+		std::cin >> i;
+		if (i == 777) break;
+		cnt++;
+
+		if (cnt%2==0){
+			if (i == prv_i){
+				k++;
+			}
+		}
+		prv_i = i;
+	}
+	std::cout << "Kol-vo ravnyh - " << k << std::endl;
+	return 0;
+}
