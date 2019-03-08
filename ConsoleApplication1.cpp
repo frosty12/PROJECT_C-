0bbc60ffe652cf13d03e4d08cfa43363a6984eee
frosty12 <38581029+frosty12@users.noreply.github.com>
2019-03-08 15:26:51 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,28 @@
+#include "stdafx.h"
+#include <iostream>
+
+int main(int argc, _TCHAR* argv[])
+{
+	int i, m, k, pk;
+	m = 0;
+	k = 0;
+	pk = 0;
+	while (true) {
+		std::cout << "Enter number " << std::endl;
+		std::cin >> i;
+		if (i == 777) break;
+		k++;
+		if (k == 1){
+			m = i;
+			pk = k;
+		}
+		else{
+			if (i <= m){
+				m = i;
+				pk = k;
+			}
+		}
+	}
+	std::cout << "Min nuber is " << pk << std::endl;
+	return 0;
+}
