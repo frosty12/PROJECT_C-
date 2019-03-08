10869d8191cef714f3ef156e7427d9391682cfeb
frosty12 <38581029+frosty12@users.noreply.github.com>
2019-03-08 13:25:07 +0300

    Create Srednye_arifmet.cpp

---
+++ Srednye_arifmet.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include <iostream>
+
+
+int main(){
+	int i,k,suma,srar;
+	k=0;
+	suma = 0;
+	while (true)
+	{
+		std::cout << "Enter number " << std::endl;
+		std::cin >> i;
+		if (i == 777) break;
+		suma = suma+i;
+		k++;
+	}
+	srar = suma / k;
+	std::cout << "Srednye arifmet is " << srar << std::endl;
+	return 0;
+}
